1273e438d338f4d87fc882426addf1951d34b3b6
Zhen-hao <zhenhaolee@gmail.com>
2021-03-12 11:23:35 +0100

    add make pdf

---
+++ make-pdf.sh
@@ -0,0 +1,21 @@
+# works on Mac
+
+# this works for deckjs
+decktape automatic file:///Volumes/MacP2/CHDR/datacloud/presentations/data-engineering/index.html  prentation.pdf
+
+decktape reveal file:///Volumes/MacP2/CHDR/datacloud/presentations/data-engineering/index.html  prentation.pdf
+
+
+# This works
+decktape generic --key=ArrowRight --max-slides=14 file:///Volumes/MacP2/CHDR/datacloud/presentations/data-engineering/index.html  prentation.pdf
+
+decktape automatic index.html  prentation.pdf
+
+decktape reveal --screenshots --screenshots-size=1600x1200 index.html  prentation.pdf
+
+# use this one
+decktape reveal --size='2048x1536' index.html  prentation.pdf
+
+decktape reveal --size=2048x1536 index.html  prentation.pdf
+
+decktape --screenshots --screenshots-size=1600x1200 index.html  prentation.pdf
